731127ef2b40d17a2bc147a3746028cfd02ce0be
Adrian870k <montoyaquinjordan@gmail.com>
2025-11-14 23:57:40 -0500

    se agrega la base de los micro servicios

---
+++ servicio-cuentas/src/main/java/com/banco/clientes/servicio_cuentas/ServicioCuentasApplication.java
@@ -0,0 +1,13 @@
+package com.banco.clientes.servicio_cuentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioCuentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioCuentasApplication.class, args);
+	}
+
+}
